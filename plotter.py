3da494ec3ce925a1cbcb9e27cc0dc9a29414103d
Simon Fojtu <simon.fojtu@gmail.com>
2019-10-24 10:32:54 +0200

    plotter: progressbar, lines drawing

--- plotter.py
+++ plotter.py
@@ -1,6 +1,7 @@
 import argparse
 import itertools
 import pprint
+import progressbar
 import random
 import requests
 import time
@@ -19,11 +20,15 @@ class APIError(Exception):
 
 
 class FirenodejsAPI:
-    def __init__(self, url, fake_move=False):
-        self._url = url
-        self._fake_move = fake_move
+    def __init__(self, args):
+        self._url = args.url
+        self._fake_move = args.fakemove
+        if args.tv is not None:
+            self.position({'sys':{'tv': args.tv}})
+        if args.mv is not None:
+            self.position({'sys':{'mv': args.mv}})
 
-        if not fake_move:
+        if not self._fake_move:
             response = self.position(request={'sys':''})
             pp = pprint.PrettyPrinter(indent=4)
             pp.pprint(response)
@@ -47,7 +52,7 @@ class FirenodejsAPI:
             resp = requests.post(self._url + '/firestep', json=request)
             if resp.status_code != 200:
                 raise APIError('POST {}/firestep json={} -> {}'.format(self._url, request, resp.status_code))
-            #time.sleep(0.1)
+            time.sleep(0.1)
             return resp.json()
 
 
@@ -106,37 +111,17 @@ def chessboard(center, size):
         draw_rectangle(x * a, y * a, (x + 1) * a, (y + 1) * a, fill=True)
 
 
-#def draw_bitmap(api, args):
-#    img = cv2.imread(args.image, cv2.IMREAD_GRAYSCALE)
-#    bitmap(api, img, (0, 0), scale=1.0)
-#
-#
-#def bitmap(api, img, center, *, threshold=128, scale=1, h_flip=True):
-#    '''
-#    Draws binary image using dots where a pixel value is less than threshold
-#    1px = 1mm^2
-#    '''
-#    assert len(img.shape) == 2
-#    if scale != 1:
-#        img = cv2.resize(img, None, fx=scale, fy=scale)
-#    h, w = img.shape
-#    x0, y0 = center - np.asarray((int(w / 2), int(h / 2)))
-#    print('Drawing bitmap {}x{} at {}:{}'.format(w, h, x0, y0))
-#    if h_flip:
-#        img = img[:, ::-1]
-#    for r in range(h):
-#        set_z(api, 0)
-#        for c in range(w):
-#            if img[r, c] < threshold:
-#                api.position({'mov': {'x': int(c + x0), 'y': int(r + y0)}})
-#                set_z(api, -10)
-#                set_z(api, -5)
-#        set_z(api, 0)
-
-
 def draw_bitmap(api, args):
-    # TODO hflip
     img = cv2.imread(args.image, cv2.IMREAD_GRAYSCALE)
+    if args.hflip:
+        img = img[:, ::-1]
+    if args.dots:
+        draw_bitmap_dots(api, img, args)
+    if args.lines:
+        draw_bitmap_lines(api, img, args)
+
+
+def draw_bitmap_dots(api, img, args):
     height, width = img.shape[:2]
     dots = bitmap_2_dots(img, threshold=args.threshold)
     x0, y0 = tuple(map(float, args.offset.split(':')))
@@ -155,7 +140,14 @@ def draw_bitmap(api, args):
     if args.random:
         random.shuffle(dots)
 
-    draw_dots(api, dots)  # z_up, z_down
+    widgets=[
+        ' [', progressbar.Timer(), '] ',
+        progressbar.Bar(),
+        ' (', progressbar.ETA(), ') ',
+    ]
+    bar = progressbar.ProgressBar(max_value=len(dots), widgets=widgets).start()
+    draw_dots(api, dots, z_up=args.z_up, z_down=args.z_down, cback=bar.update)
+    bar.finish()
 
 
 def bitmap_2_dots(img, *, threshold=128):
@@ -167,51 +159,94 @@ def bitmap_2_dots(img, *, threshold=128):
             yield (c, r)
 
 
-def draw_dots(api, dots, *, z_up=5, z_down=-10):
-    for x, y in dots:
+def draw_dots(api, dots, *, z_up, z_down, cback=None):
+    for i, (x, y) in enumerate(dots):
         api.position({'mov': {'x': x, 'y': y}})
         set_z(api, z_down)
         set_z(api, z_up)
+        if cback is not None:
+            cback(i)
 
 
-def bitmap_lines(img, center, *, threshold=128, scale=1, h_flip=True):
-    '''
-    Draws binary image using horizontal lines where a line is present if pixel value is less than threshold
-    1px = 1mm^2
-    '''
+def draw_bitmap_lines(api, img, args):
+    height, width = img.shape[:2]
+    lines = bitmap_2_lines(img, threshold=args.threshold)
+    off_x, off_y = tuple(map(float, args.offset.split(':')))
+    if args.center:
+        off_x = off_x - width / args.dpmm / 2
+        off_y = off_y - height / args.dpmm / 2
+
+    print('Drawing bitmap: {}x{}px, {}x{}mm, offset: {}:{}mm'.format(
+        width, height, width / args.dpmm, height /args.dpmm, off_x, off_y
+    ))
+    time.sleep(2)
+
+    # apply scale and offset
+    lines = list((
+        ((x0 / args.dpmm + off_x, y0 / args.dpmm + off_y), (x1 / args.dpmm + off_x, y1 / args.dpmm + off_y))
+        for ((x0, y0), (x1, y1)) in lines
+    ))
+
+    if args.random:
+        random.shuffle(lines)
+
+    widgets=[
+        ' [', progressbar.Timer(), '] ',
+        progressbar.Bar(),
+        ' (', progressbar.ETA(), ') ',
+    ]
+    bar = progressbar.ProgressBar(max_value=len(lines), widgets=widgets).start()
+    draw_lines(api, lines, z_up=args.z_up, z_down=args.z_down, cback=bar.update)
+    bar.finish()
+
+
+def bitmap_2_lines(img, *, threshold=128):
+    """Convert a bitmap to a set of horizontal lines."""
     assert len(img.shape) == 2
-    if scale != 1:
-        img = cv2.resize(img, None, fx=scale, fy=scale)
     h, w = img.shape
-    x0, y0 = center - np.asarray((int(h / 2), int(w / 2)))
-    print('Drawing bitmap {}x{} at {}:{}'.format(w, h, x0, y0))
-    if h_flip:
-        img = img[:, ::-1]
     for r in range(h):
-        row = img[r, :]
-        one_indices = numpy.where(row >= threshold)
-        zero_indices = numpy.where(row < threshold)
-        if zero_indices[0] > one_indices[0]:
-            one_indices = one_indices[1:]
-        
-        set_z(0)
-        for (begin, end) in zip(zero_indices, one_indices):
-            print(begin, end)
-            api.position({'mov': {'x': int(begin + x0), 'y': int(r)}})
-            set_z(-10)
-            api.position({'mov': {'x': int(end + x0), 'y': int(r)}})
-            set_z(-5)
-        set_z(0)
+        begin = None
+        end = None
+        for c in range(w):
+            if img[r, c] < threshold:
+                if begin is None:
+                    begin = c
+                elif end is not None:
+                    yield ((begin, r), (end - 1, r))
+                    begin = None
+                    end = None
+            if img[r, c] >= threshold and begin is not None and end is None:
+                end = c
+        if begin is not None and end is not None:
+            yield ((begin, r), (end - 1, r))
+
+
+def draw_lines(api, lines, *, z_up, z_down, cback=None):
+    set_z(api, z_up)
+    for i, ((x0, y0), (x1, y1)) in enumerate(lines):
+        api.position({'mov': {'x': x0, 'y': y0}})
+        set_z(api, z_down)
+        api.position({'mov': {'x': x1, 'y': y1}})
+        set_z(api, z_up)
+        if cback is not None:
+            cback(i)
 
 
 parser = argparse.ArgumentParser(description='Tool for drawing via firenodejs', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 subparsers = parser.add_subparsers()
 parser.add_argument('--url', default='http://10.0.0.10:8080', help='URL of firenodejs')
 parser.add_argument('--fakemove', action='store_true', help='Fake movement')
+parser.add_argument('--z-up', type=float, default=15, help='Retract tip height')
+parser.add_argument('--z-down', type=float, default=-10, help='Draw tip height')
+parser.add_argument('--tv', type=float, default=0.7, help='Set seconds to reach maximum velocity')
+parser.add_argument('--mv', type=float, default=12800, help='Set maximum velocity (pulses/second)')
 
 parser_bitmap = subparsers.add_parser('bitmap', help='Bitmap drawing')
 parser_bitmap.add_argument('--image', help='Path to image', required=True)
-parser_bitmap.add_argument('--threshold', help='Threshold for binarizing an image', default=128)
+parser_bitmap.add_argument('--dots', help='Draw using dots', action='store_true')
+parser_bitmap.add_argument('--lines', help='Draw using lines', action='store_true')
+parser_bitmap.add_argument('--hflip', help='Flip the image horizontally (around vertical axis)', action='store_true')
+parser_bitmap.add_argument('--threshold', help='Threshold for binarizing an image', default=128, type=int)
 parser_bitmap.add_argument('--dpmm', type=float, help='Dots per mm', default=1)
 parser_bitmap.add_argument('--offset', help='Offset the image [mm]', default='0:0')
 parser_bitmap.add_argument('--center', help='Center image around 0:0', action='store_true')
@@ -222,11 +257,8 @@ parser_chess = subparsers.add_parser('chess', help='Chessboard drawing')
 parser_chess.add_argument('--size', help='Width of the chessboard', default=80)
 parser_chess.set_defaults(func=draw_chessboard)
 
-
 args = parser.parse_args()
-
-api = FirenodejsAPI(args.url, args.fakemove)
-
+api = FirenodejsAPI(args)
 args.func(api, args)
 
 
